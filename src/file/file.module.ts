7234b9ec1b6d21696eb3c6ee55441a29e2880952
Sebastian Kalicki <sebastian@kalicki.email>
2025-01-16 02:20:08 +0100

    fix(module): added import of File entity in file.module.ts
    - Added import of the `File` entity in `file.module.ts` to ensure correct usage within the `TypeOrmModule`.

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { File } from './file.entity';
 import { FileController } from './file.controller';
 import { FileService } from './file.service';
 
